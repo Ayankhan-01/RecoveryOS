e042e9af64282445308d315bfc0ca9cbe5a550fa
Ayan Khan <ayankhan9302295195@gmail.com>
2026-08-29 20:24:57 +0530

    Fix ML model path

--- backend/app/services/ml_predictor.py
+++ backend/app/services/ml_predictor.py
@@ -4,6 +4,14 @@ import joblib
 import pandas as pd
 
 
+BASE_DIR = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "..",
+    )
+)
+
 MODEL_PATH = os.path.join(
     BASE_DIR,
     "ml",
